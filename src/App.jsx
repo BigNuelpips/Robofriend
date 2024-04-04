5cefe9247d3da2f294255f30a8c9b7b7db702048
BigNuelpips <idakwoemmanuel300@gmail.com>
2024-04-04 14:43:56 +0100

    workouut

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Search from './assets/Component/Seacrch'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Search/>
+      
+  )
+}
+
+export default App
